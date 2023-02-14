e02fb064f92305c7d956023bf2c73c54b8ba3720
Smith <olaoluwasamsmith@gmail.com>
2023-02-14 20:05:27 +0100

    postgres

--- simbiglobal/settings.py
+++ simbiglobal/settings.py
@@ -85,11 +85,19 @@ WSGI_APPLICATION = 'simbiglobal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
